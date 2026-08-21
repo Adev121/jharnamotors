5c9a2d38c4971aef81c128add48154758fb681d8
Adev121 <kumarramabhishek89@gmail.com>
2026-08-21 17:19:56 +0530

    Fixed

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,8 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "https://jharnamotors-backend.onrender.com",
-        "https://jharnamotors.onrender.com/",
+        "https://jharnamotors.onrender.com",
+        "https://jharnamotors.onrender.com/*",
         "https://jharnamotors-frontend.onrender.com"
     ],
 
